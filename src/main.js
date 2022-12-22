d5b62154d1483564e6f0d7fb09061dd0b84e576e
Socheath-Horl <horl.socheath@gmail.com>
2022-12-22 08:29:51 +0700

    home page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import axios from 'axios'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
+createApp(App).use(store).use(router, axios).mount('#app')
